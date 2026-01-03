a29450abe6e5a42aa56e7857de769a24b10dc389
Voitke <wojtek.jedi@gmail.com>
2026-01-03 23:38:39 +0100

    add endpoint fetching all timereports in database

--- backend/src/main/java/io/github/voitke/timereportingapp/timereport/TimeReportController.java
+++ backend/src/main/java/io/github/voitke/timereportingapp/timereport/TimeReportController.java
@@ -6,15 +6,14 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Optional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +23,14 @@ public class TimeReportController {
     TimeReportRepository timeReportRepository;
     @Autowired
     TimeReportService timeReportService;
+
+    @GetMapping("/api/timereports")
+    public Page<TimeReport> getAllReports(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        Pageable pageable = PageRequest.of(page,size);
+        return timeReportRepository.findAll(pageable);
+    }
 
     @GetMapping("/api/timereport/{id}")
     public ResponseEntity<TimeReport> getReportById(@PathVariable Long id) {
