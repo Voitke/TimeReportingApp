c2b45323be68aaa882a8902e9e187f555fc28ef7
Vikki <wojtek.jedi@gmail.com>
2026-01-05 23:16:49 +0100

    add DTO validation

--- backend/src/main/java/io/github/voitke/timereportingapp/timereport/model/TimeReportDTO.java
+++ backend/src/main/java/io/github/voitke/timereportingapp/timereport/model/TimeReportDTO.java
@@ -1,13 +1,19 @@
 package io.github.voitke.timereportingapp.timereport.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.github.voitke.timereportingapp.timereport.validation.PositiveDuration;
+import jakarta.validation.constraints.PastOrPresent;
+
 import java.time.Duration;
 import java.time.LocalDate;
 
 public record TimeReportDTO(
     @JsonProperty("user_id") Long userId,
     @JsonProperty("activity_id") Long activityId,
+    @PastOrPresent
     LocalDate date,
+    @PositiveDuration
     Duration duration,
+    @PastOrPresent
     LocalDate dateReported
 ) {}
